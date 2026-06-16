4e62c0097cc90fc189c73bdbd6307d718c2e7d1d
Ch0iHanSam <choigo020419@gmail.com>
2026-06-16 17:35:45 +0900

    자이로 인터페이스

--- Assets/ReFit_Final/00.Commons/IReFitGyro.cs
+++ Assets/ReFit_Final/00.Commons/IReFitGyro.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
-public class IReFitGyro : MonoBehaviour
+//씬마다 자이로센서의 상하좌우입력의 기능을 구현하는 인터페이스
+public interface IReFitGyro
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //씬마다 상황이 다르니 기본형 처리
+    void GyroInputUp() { }
+    void GyroInputDown() { }
+    void GyroInputLeft() { }
+    void GyroInputRight() { }
+    void GyroInputCenter() { }
 }
